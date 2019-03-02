9aa6c02e2b446529f3b7e2db0930b750b279c6dc
Anita Virag <ik.virag@gmail.com>
2019-03-02 23:42:52 +0100

    approach 1: provider, consumer for products page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import ShopContext from './context/shop-context';
 import ProductsPage from './pages/Products';
 import CartPage from './pages/Cart';
 import './App.css';
 
 class App extends Component {
+  state = {
+    products: [
+      { id: 'p1', title: 'Gaming Mouse', price: 29.99 },
+      { id: 'p2', title: 'Harry Potter 3', price: 9.99 },
+      { id: 'p3', title: 'Used plastic bottle', price: 0.99 },
+      { id: 'p4', title: 'Half-dried plant', price: 2.99 }
+    ],
+    cart: []
+  };
+
+  addProductToCart = product => {
+    console.log('Adding product...');
+  };
+  removeProductFromCart = productId => {
+    console.log('Remove product #' + productId);
+  };
+
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={ProductsPage} exact />
-          <Route path="/cart" component={CartPage} exact />
-        </Switch>
-      </BrowserRouter>
+      <ShopContext.Provider
+        value={{
+          products: this.state.products,
+          cart: this.state.cart,
+          addProductToCart: this.addProductToCart,
+          removeProductFromCart: this.removeProductFromCart
+        }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={ProductsPage} exact />
+            <Route path="/cart" component={CartPage} exact />
+          </Switch>
+        </BrowserRouter>
+      </ShopContext.Provider>
     );
   }
 }
