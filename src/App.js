39d722a9fbd59caa2e2f15f2cc0c748ad017a8b6
Anita Virag <ik.virag@gmail.com>
2019-03-02 23:56:49 +0100

    approach 2: static contextType

--- src/App.js
+++ src/App.js
@@ -19,9 +19,43 @@ class App extends Component {
 
   addProductToCart = product => {
     console.log('Adding product...');
+
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === product.id
+    );
+
+    if (updatedItemIndex < 0) {
+      updatedCart.push({ ...product, quantity: 1 });
+    } else {
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex]
+      };
+      updatedItem.quantity++;
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+
+    this.setState({cart: updatedCart});
   };
   removeProductFromCart = productId => {
     console.log('Remove product #' + productId);
+
+    const updatedCart = [...this.state.cart];
+    const updatedItemIndex = updatedCart.findIndex(
+      item => item.id === productId
+    );
+
+    const updatedItem = {
+      ...updatedCart[updatedItemIndex]
+    };
+    updatedItem.quantity--;
+    if (updatedItem.quantity <= 0) {
+      updatedCart.splice(updatedItemIndex, 1);
+    } else {
+      updatedCart[updatedItemIndex] = updatedItem;
+    }
+
+    this.setState({cart: updatedCart});
   };
 
   render() {
